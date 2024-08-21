c566c437ab662102ac610c91f2755f19acfa45f6
Skater901 <skaterx901@hotmail.com>
2024-08-21 16:12:04 +1000

    Initial commit of coroutines-exception-bug.

---
+++ src/main/kotlin/au/com/skater901/Main.kt
@@ -0,0 +1,29 @@
+package au.com.skater901
+
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+
+fun main() {
+    System.setProperty("kotlinx.coroutines.debug", "on")
+    try {
+        runBlocking {
+            withContext(coroutineContext) {
+                throw IllegalArgumentException(RuntimeException("oh no!"))
+            }
+        }
+    } catch (t: Throwable) {
+        t.print()
+    }
+}
+
+private fun Throwable.print() {
+    println("${javaClass.name}: $message")
+    print("  ")
+    println(stackTrace.joinToString("\n  "))
+    cause?.let {
+        if (it != this) {
+            print("Caused by: ")
+            it.print()
+        }
+    }
+}
